99f2d2c886963070526968b464a01dcb92769692
Senul55 <144893361+Senul55@users.noreply.github.com>
2024-05-06 14:41:38 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+# Setting page configuration
+st.set_page_config(
+    page_title="Global Superstore",
+    page_icon=":chart_with_upwards_trend:"
+)
+# Sample data
+df = pd.read_csv("Processed_GlobalSuperstoreLite.csv")
+
+
+# Main title
+st.title("Sales Dashboard")
