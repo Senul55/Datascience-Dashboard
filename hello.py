2d5920f5ab7bc56623c8f44a639610ce370f2b75
Senul55 <144893361+Senul55@users.noreply.github.com>
2024-05-06 21:29:06 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px  # Import Plotly Express for visualizations
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # Setting page configuration
 st.set_page_config(
