36ea0868bd404e229cf2b69000fbaf35aec895ba
Senul55 <144893361+Senul55@users.noreply.github.com>
2024-05-06 15:46:29 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 # Setting page configuration
 st.set_page_config(
-    page_title="Global Superstore",
+    page_title="Global Superstoree",
     page_icon=":chart_with_upwards_trend:"
 )
 
