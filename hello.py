47f67f0f95c5bae057e030644c790e0cf36f50ba
Senul55 <144893361+Senul55@users.noreply.github.com>
2024-05-07 07:57:22 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Setting page configuration
 st.set_page_config(
     page_title="Global Superstore Data Sales Dashboard",
-    st.header("Global Superstore Dashboard")
+st.header("Global Superstore Dashboard")
     page_icon=":chart_with_upwards_trend:",
     layout="wide",  # Adjust layout as needed
 )
